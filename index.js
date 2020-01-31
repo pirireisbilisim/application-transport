79c80cf8dfddb9263c9e76f76fe5707d37e0109b
Kürşat Yiğitoğlu <kursat@pirireis.com.tr>
2020-01-31 09:26:27 +0300

    class

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-module.exports = {
-    sendMessage: (iframeRef, appUrl, coordinates, sources) => {
-        if (iframeRef.current) {
-            iframeRef.current.contentWindow.postMessage(
-                {type: 'coordinates', value: coordinates, sources: sources ? sources : ""},
-                appUrl,
-            );
-        }
-    },
-
-    getMessage: (ev) => {
-        ev.addEventListener('message', function(event) {
-            return event.data;
-        }, false);
-    }
+const EventTypes = {
+    POINT_SELECTED: 'POINT_SELECTED',
 };
+
+class Transport {
+    constructor(id) {
+        this.iframeId = id;
+    }
+
+    sendMessage () {
+        const iframe = document.getElementById(this.iframeId);
+        iframe.contentWindow.postMessage(
+            {type: 'coordinates', value: coordinates},
+            appUrl,
+        );
+
+    }
+
+    subscribeToMessages(onMessage) {
+        window.addEventListener('message', onMessage, false);
+    }
+}
+
+module.exports = {EventTypes, Transport}
